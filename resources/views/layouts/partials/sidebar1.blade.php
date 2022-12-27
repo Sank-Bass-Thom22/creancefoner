1f99c16a2b485ef4e77b0671f0515a482b2f0f2e
Nana Zakaria Ben <53193712+zben99@users.noreply.github.com>
2022-12-27 13:25:02 +0000

    MAJ debtor 1.5

--- resources/views/layouts/partials/sidebar1.blade.php
+++ resources/views/layouts/partials/sidebar1.blade.php
@@ -1,13 +1,23 @@
 <div class="nk-sidebar">           
             <div class="nk-nav-scroll">
                 <ul class="metismenu" id="menu">
-                    <li class="nav-label"> Tableau de bord</li>
+                <li class="nav-label">Tableau de bord</li>
+                        <a href="/" aria-expanded="false">
+                            <i class="icon-speedometer menu-icon"></i><span class="nav-text"> Tableau de bord</span>
+                        </a>
+                    <li>
+
+                    </li>
                     <a href="{{ route('myloans') }}" aria-expanded="false">
                             <i class="icon-speedometer menu-icon"></i><span class="nav-text">  Mes prêts</span>
                     </a>
 
+                    <a href="{{ route('myrepayments') }}" aria-expanded="false">
+                            <i class="icon-speedometer menu-icon"></i><span class="nav-text"> Mes remboursements </span>
+                    </a>
+
                     <a href="{{ route('createsimulation') }}" aria-expanded="false">
-                            <i class="icon-speedometer menu-icon"></i><span class="nav-text">  Mon échéancier</span>
+                            <i class="icon-speedometer menu-icon"></i><span class="nav-text">  Simulation</span>
                     </a>
 
                     <a href="{{ route('usefuldocuments') }}" aria-expanded="false">
--- resources/views/layouts/partials/sidebar2.blade.php
+++ resources/views/layouts/partials/sidebar2.blade.php
@@ -1,7 +1,13 @@
 <div class="nk-sidebar">           
             <div class="nk-nav-scroll">
                 <ul class="metismenu" id="menu">
-                    <li class="nav-label"> Tableau de bord</li>
+                <li class="nav-label">Tableau de bord</li>
+                        <a href="/" aria-expanded="false">
+                            <i class="icon-speedometer menu-icon"></i><span class="nav-text"> Tableau de bord</span>
+                        </a>
+                    <li>
+
+                    </li>
                     <a href="{{ route('myemployes') }}" aria-expanded="false">
                             <i class="icon-speedometer menu-icon"></i><span class="nav-text"> Mes employés</span>
                     </a>
